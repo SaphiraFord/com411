6599aa7d9c10a4f2324d905eece02f4c4efe2ffb
repl.it user <replituser@example.com>
2020-09-30 11:02:49 +0000

    Added code to display ascii robot with two inputtable eyes

---
+++ 1-basics/1-input/ascii_robot.py
@@ -0,0 +1,7 @@
+leftEye = input("Please enter a character for the left eye")
+rightEye = input("Please enter a character for the right eye")
+
+print("##########")
+print("# {}   {}  #".format(leftEye, rightEye))
+print("#        #")
+print("##########")  
