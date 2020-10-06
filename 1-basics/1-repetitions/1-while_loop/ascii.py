9821ccb36797be52c48cae12b3f2b7ed0a932cf4
SaphiraFord <5FORDS32@solent.ac.uk>
2020-10-06 21:55:16 +0000

    Added code to demonstrate the use of a while loop with ascii art.

---
+++ 1-basics/1-repetitions/1-while_loop/ascii.py
@@ -0,0 +1,14 @@
+#Getting the user to input how many bars must be charged and storing this 
+print("How many bars should be charged?")
+bars = int(input())
+
+#instantiating variable chargedBars to count how many bar have been charged with this variable being set to 1 initially and when the chargedBars are less than the user bars then the program will stop. However while the chargedBars are less than the user inputted bars then it will print charging... (current number of chargedBars) and add 1 onto the chargedBars value so that the iteration will eventually come to a stop. Then it will output a message saying that the battery is fully charged
+
+chargedBars = 0
+
+while(chargedBars < bars):
+  print("Charging:...", end="")
+  chargedBars = chargedBars + 1
+  print(chargedBars * " █ ")
+
+print("The battery is fully charged.")
