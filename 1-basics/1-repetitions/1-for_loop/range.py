656365c23a61d295e2e8390c60122c2fa3f32fc0
SaphiraFord <5FORDS32@solent.ac.uk>
2020-10-07 12:59:32 +0000

    Added code to demonstrate the use of a simple for loop.

---
+++ 1-basics/1-repetitions/1-for_loop/range.py
@@ -0,0 +1,11 @@
+#Getting the user to input how what level of brightness is required and storing this 
+print("What level of brightness is required?")
+brightnessLevel = int(input())
+
+print("Adjusting brightness...")
+#A for iteration that for the brightnessLevel in the range of the inputted brightnessLevel it will count only the even numbers within that range given between 0 and the input. Then it will output the brightness level only in even outputs such as 0, 2, 4, and 6 if the number inputted was 6. brightness level will +1 every iteration until it is at the number inputted.
+
+for brightnessLevel in range(0, brightnessLevel + 1, 2):
+ print("Beep's brightness level: ", "*" * brightnessLevel)
+ print("Bop's brightness level: ", "*" * brightnessLevel)
+print("Adjustments complete!")
