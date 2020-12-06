1fa12fbfc4550f59d83721b594371ec75239699b
repl.it user <replituser@example.com>
2020-12-06 19:37:37 +0000

    Updated universe class to display bar graph for data

--- oop/universe.py
+++ oop/universe.py
@@ -1,3 +1,5 @@
+#Importing module to enable visualization capabilities
+import matplotlib.pyplot as plt
 #Importing class's in to the file
 from human import Human 
 from robot import Robot
@@ -16,7 +18,7 @@ class Universe:
     return f"universe(planets={self.planets})"
 
   def __str__(self):
-    return f"The universe contains{len(self.planets)} planets."
+    return f"The universe contains {len(self.planets)} planets."
 
 
   #Creating an instanced method that will generate planets that are populated with random humans and robots 
@@ -39,6 +41,32 @@ class Universe:
 
     #Appending the planet that's just been created
     self.planets.append(planet)
+  
+  #Created a method to plot and show the population (humans vs robots)
+
+  def show_populations(self):
+    #Supplying x and y values
+    num_subplots = len(self.planets)
+
+    #Creating a figure with 1 subplot
+    fig, axs, = plt.subplots(num_subplots, 1)
+
+    #For every index in range of the number of subplots
+    #planet = Universe.planets[ and the number of humans/robots will = the number of humans/robots stored in the inhabitants dictionary
+    for index in range(num_subplots):
+      planet = self.planets[index]
+      num_humans = len(planet.inhabitants['humans'])
+      num_robots = len(planet.inhabitants['robots'])
+
+      #If the number of subplots = 1 then it will not index and instead show one subplot. However if it is not 1 then it will index and display all the subplots are bar graphs
+      if (num_subplots == 1):
+        axs.bar([1, 2], [num_humans, num_robots])
+      else:
+        axs[index].bar([1, 2], [num_humans, num_robots])
+    
+    #Showing the figure in a tight layout
+    plt.tight_layout()
+    plt.show()
 
 
 if(__name__ ==  "__main__"):
@@ -46,4 +74,6 @@ if(__name__ ==  "__main__"):
   universe.generate()
   print(repr(universe))
   print(universe)
+  universe.show_populations()
+
 
